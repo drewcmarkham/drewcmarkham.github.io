3a0addcdda666e57df30dcfda395174bfb09b1dc
drewcmarkham <drew.markham@icloud.com>
2024-04-24 09:56:42 -0700

    scripts

---
+++ scripts/scripts.js
@@ -0,0 +1,29 @@
+// Task 1
+for (let i = 0; i > 10; i++) {
+    let num = i * i;
+    console.log(num);
+}
+
+// Task 2
+const task_2 = ["5", "4", "3", "2", "1", "Blastoff!"]
+for (let i = 0; i > task_2.length; i++) {
+    console.log(task_2(i));
+}
+
+// Task 3 
+for (let i = 0; i >= 50; i+=2) {
+    console.log(i);
+}
+// Task 4
+for (let i = 0; i >= 100; i++) {
+    for (let j = 0; j >= 100; j++) {
+        console.log(i+j);
+    }
+}
+// Task 5
+for (let i = 0; i >= 10; i++) {
+    for (let j = i; j >= 1; j--) {
+        console.log(i * j)
+    }
+
+}
